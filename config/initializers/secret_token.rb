cbe8cd3831a4ba4d62c655946e92a68236d68b0d
Max <msprior@gmail.com>
2010-10-01 03:09:55 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheArtsBlock::Application.config.secret_token = '93332d12e24cc32fc74e8fdf31c2b38d30edf2d1e4954c8f17a57c90a4c9f0a904a7a1e67b99959455a816284f957952d75c48f48e92c18ff6135bf574eec61c'

3066cd4f7817f1a09306bdab482fbe16f7a57f68
Max <msprior@gmail.com>
2010-12-09 22:36:18 -0500

    sumpin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101209220946) do
+ActiveRecord::Schema.define(:version => 20101209211001) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -46,13 +46,6 @@ ActiveRecord::Schema.define(:version => 20101209220946) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "quantity",   :default => 1
-  end
-
-  create_table "menus", :force => true do |t|
-    t.string   "name"
-    t.string   "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "payment_notifications", :force => true do |t|

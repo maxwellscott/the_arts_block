c5efe067a9046c8dad595faa59b97a63e79356bd
Nellie Prior <nprior@dhcp-0138248363-8d-84.client.student.harvard.edu>
2011-01-24 21:10:47 -0500

    root

--- config/routes.rb
+++ config/routes.rb
@@ -84,7 +84,7 @@ Boxoffice::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
-  # root :to => 'listing#index', :as => 'listing'
+  root :to => 'calendar#index', :as => 'tickets'
 
   # See how all your routes lay out with "rake routes"
 
